24d891ef47b5ffbadff5a1c7821f585adc3af8a1
Artem Velmozhin <artemvrabota@yandex.ru>
2022-07-15 07:15:58 +0400

    feat: techs адаптив

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,6 +3,7 @@ import React from "react";
 import Header from "../Header/Header";
 import Promo from "../Promo/Promo";
 import AboutProject from "../AboutProject/AboutProject";
+import Techs from "../Techs/Techs";
 
 import "./Main.css";
 
@@ -12,6 +13,7 @@ function Main(props) {
       <Header loggedIn={props.loggedIn} />
       <Promo />
       <AboutProject />
+      <Techs />
     </main>
   );
 }
